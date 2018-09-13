5bcd022bab9449e6e0e1098e1ff6448bc814d5fc
mwoolery <mwoolery@nwmissouri.edu>
2018-09-13 13:52:58 -0500

    modified the header to have right color instead of just black, need to add in logo later.

--- src/App.js
+++ src/App.js
@@ -3,49 +3,26 @@ import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
-const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
-    </div>
-);
-
-const Home = (props) => (
-  <Page title="Home"/>
-);
-
-const About = (props) => (
-  <Page title="About"/>
-);
-
-const Settings = (props) => (
-  <Page title="Settings"/>
-);
-
 class App extends Component {
   render() {
     return (
-      <Router history={browserHistory}>
-        <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/settings" component={Settings}/>
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <a href = "http://www.nwmissouri.edu">
+          <img src= "../northwest.png" className="App-logo" alt="logo" />
+         </a>
+          <h1 className="App-title">Hughes FieldHouse</h1>
+        </header>
+        
+        
+        
+      </div>
+     
     );
   }
 }
+
+
+
 
 export default App;
