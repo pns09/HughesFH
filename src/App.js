2e89b7d848a0a39e79b5957a3ca6dcfb96b0547e
mwoolery <mwoolery@nwmissouri.edu>
2018-09-13 14:39:21 -0500

    added in the LinkTileBox component to App.js uses css grid to make the box of tiles.  Currently just stub values for the tiles since we need to acquire some pictures for the tabs and put in the updated links ourselves, so it does not look good yet but it sets up the template some more.

--- src/App.js
+++ src/App.js
@@ -13,14 +13,68 @@ class App extends Component {
          </a>
           <h1 className="App-title">Hughes FieldHouse</h1>
         </header>
-        
-        
-        
+        <LinkTileBox/>
       </div>
-     
+      
     );
   }
 }
+
+
+
+//Link Tile Box is the box of links you see on hughes fieldhouse current site
+//Could break down each component into smaller tiles but I think the LinkTileBox is only used on home anyways
+class LinkTileBox extends Component {
+  render() {
+    return (
+      <div class="grid-container">
+        <div class="grid-item" >
+          <a href="experience.htm" target="_self" title="Title">
+          <img alt="Title" height="136px" src="images/Title.jpg" width="287px"/>
+          <h3>Title</h3>
+          </a>
+        </div>  
+        <div class="grid-item" >
+          <a href="experience.htm" target="_self" title="Title">
+          <img alt="Title" height="136px" src="images/Title.jpg" width="287px"/>
+          <h3>Title</h3>
+          </a>
+        </div> 
+        <div class="grid-item" >
+          <a href="experience.htm" target="_self" title="Title">
+          <img alt="Title" height="136px" src="images/Title.jpg" width="287px"/>
+          <h3>Title</h3>
+          </a>
+        </div> 
+        <div class="grid-item" >
+          <a href="experience.htm" target="_self" title="Title">
+          <img alt="Title" height="136px" src="images/Title.jpg" width="287px"/>
+          <h3>Title</h3>
+          </a>
+        </div>  
+        <div class="grid-item" >
+          <a href="experience.htm" target="_self" title="Title">
+          <img alt="Title" height="136px" src="images/Title.jpg" width="287px"/>
+          <h3>Title</h3>
+          </a>
+        </div> 
+        <div class="grid-item" >
+          <a href="experience.htm" target="_self" title="Title">
+          <img alt="Title" height="136px" src="images/Title.jpg" width="287px"/>
+          <h3>Title</h3>
+          </a>
+        </div> 
+        
+        
+      </div>
+
+    );
+  }
+}
+
+
+
+
 
 
 
